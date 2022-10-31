ae14038f5b5bdeb0d6c39e58ffa4472ee6aaae70
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-10-31 16:24:30 -0300

    Exercício Média

---
+++ 1.Exercicio_media.py
@@ -0,0 +1,55 @@
+# Identificar os diferentes tipos de operadores
+# Cada exercício tem declaração de variáveis e, na sequência, uma setença
+# que usa as variáveis e operadores lógicos. Determine qual é o resultad final
+# (Verdadeiro ou Falso) de cada sentença:
+
+# Sentença 01
+# nota = 10
+
+# if nota <= 10:
+#   print("Verdadeiro")
+# else:
+#   print("Falso")
+  
+# Sentença 02
+# nota = 6 
+# faltas = 4
+
+# if nota <= 6 and faltas <=3:
+#   print("Verdedeiro")
+# else:
+#   print("Falso")
+  
+# Sentença 03
+# convidados = 3 
+# fumante = False 
+
+# if convidados < 4 or fumante == True:
+#   print("Verdadeiro")
+# else:
+#   print("Falso")
+  
+# Sentença 04
+# dia = "qua"
+
+# if dia == "sab" or dia == "dom":
+#   print("Verdedeiro")
+# else:
+#   print("Falso")
+
+# Sentença 05
+# feriado = True 
+
+# if not feriado == False:
+#   print("Verdadeiro")
+# else:
+#   print("Falso")
+  
+# Sentença 06
+# dia = "ter"
+# feriado = False 
+
+# if dia == "seg" or  not feriado == False:
+#   print("Verdedeiro")
+# else:
+#   print("Falso")
